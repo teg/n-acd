046330b698f0079a03e43206d31bd97ab9c02bf1
Tom Gundersen <teg@jklm.no>
2017-11-09 17:56:19 +0100

    n-acd: remotable API rework
    This is the start of a total rework of the networking APIs, with the aim
    of making the APIs "IPC transparent". That firstly means ensuring the
    API calls are not inherently synchronous and not unreasonably fine-grained.
    The internals will mostly remain the same, but the APIs will be changed as
    described below.
    
    * Static configuration
    
    Rather than having a colleciton of individual setters, all configuration
    is provided to the engine when calling start(), and in order to change
    the configuration the engine must be restarted. The configuration is
    given as an open-coded struct.
    
    * Events
    
    Rather than registering callbacks that are triggered from dispatch(),
    dispatch() pushes pending events onto a queue, which must be flushed
    by calling pop_event() until it runs empty. Each event is represented
    as an enum together with an open-coded struct providing data associated
    with the event.
    
    * Errors
    
    Appart from -EAGAIN, all negative error codes returned by the library
    are fatal, and should not be inspected (apart from as a primitive
    debugging hint).
    
    * Getters and setters
    
    Whenever possible these are avoided, as all config should be set as
    described above, and the caller should cache information rather than
    querying the library. All information is still available to the caller
    either because they set it, or because it was given in a callback.
    
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- src/test-loopback.c
+++ src/test-loopback.c
@@ -8,43 +8,42 @@
 #include <stdlib.h>
 #include "test.h"
 
-static void test_loopback_fn(NAcd *acd, void *userdata, unsigned int event, const struct ether_arp *conflict) {
-        bool *running = userdata;
-
-        assert(event == N_ACD_EVENT_READY);
-        *running = false;
-}
-
 static void test_loopback(int ifindex, const struct ether_addr *mac) {
+        NAcdConfig config = {
+                .ifindex = ifindex,
+                .mac = *mac,
+                .ip = { htobe32((192 << 24) | (168 << 16) | (1 << 0)) },
+        };
         struct pollfd pfds;
-        bool running;
         NAcd *acd;
         int r, fd;
 
         r = n_acd_new(&acd);
         assert(r >= 0);
 
-        r = n_acd_set_ifindex(acd, ifindex);
-        assert(r >= 0);
-        r = n_acd_set_mac(acd, mac);
-        assert(r >= 0);
-        r = n_acd_set_ip(acd, &(struct in_addr){ htobe32((192 << 24) | (168 << 16) | (1 << 0)) });
-        assert(r >= 0);
-
         n_acd_get_fd(acd, &fd);
-        r = n_acd_start(acd, test_loopback_fn, &running);
+        r = n_acd_start(acd, &config);
         assert(r >= 0);
 
-        for (running = true; running; ) {
+        for (;;) {
+                NAcdEvent event;
                 pfds = (struct pollfd){ .fd = fd, .events = POLLIN };
                 r = poll(&pfds, 1, -1);
                 assert(r >= 0);
 
                 r = n_acd_dispatch(acd);
                 assert(r >= 0);
+
+                r = n_acd_pop_event(acd, &event);
+                if (r >= 0) {
+                        assert(event.event == N_ACD_EVENT_READY);
+                        break;
+                } else {
+                        assert(r == -EAGAIN);
+                }
         }
 
-        n_acd_unref(acd);
+        n_acd_free(acd);
 }
 
 int main(int argc, char **argv) {
